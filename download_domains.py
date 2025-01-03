c17c8790eea4d117c9be071e4a51202491cd8940
Rémy Grünblatt <remy@grunblatt.org>
2025-01-03 21:57:00 +0100

    Switch vers EduGain

--- download_domains.py
+++ download_domains.py
@@ -6,10 +6,12 @@ import urllib
 import tldextract
 from bs4 import BeautifulSoup
 
-fer = "https://pub.federation.renater.fr/metadata/fer/all.xml"
-xml = urllib.request.urlopen(fer)
-soup = BeautifulSoup(xml.read(), features="xml")
-preauthorized_domains = set([tldextract.extract(item.text).registered_domain for item in soup.find_all("shibmd:Scope")])
+urls = ["https://pub.federation.renater.fr/metadata/edugain/all.xml", "https://pub.federation.renater.fr/metadata/fer/all.xml"]
+preauthorized_domains = set()
+for url in urls:
+    xml = urllib.request.urlopen(url)
+    soup = BeautifulSoup(xml.read(), features="xml")
+    preauthorized_domains.update(set([tldextract.extract(item.text).registered_domain for item in soup.find_all("shibmd:Scope")]))
 print(f"The preauthorized domains set contains {len(preauthorized_domains)} domains.")
 with open("fer_domains.txt", "wt") as f:
     for domain in preauthorized_domains:
